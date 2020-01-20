109af759091995e8f9268982e4b4def3e9392634
numbp <o9o73286699@gmail.com>
2020-01-21 03:11:46 +0900

    past201912_b: 同位処理

--- qs/past201912_b/main.go
+++ qs/past201912_b/main.go
@@ -18,18 +18,23 @@ func main() {
 			continue
 		}
 		if 0 < len(sales) {
-			fmt.Printf("today sale rank %d in Sales\n", ranking(sale, sales))
+			rank, diff := ranking(sale, sales)
+			fmt.Printf("today sale rank%d and diff%d in Sales\n", rank, diff)
 		}
 		sales = append(sales, sale)
 	}
 }
 
-func ranking(n int, num []int) int {
-	sort.Ints(num)
+func ranking(n int, num []int) (rank, diff int) {
+	diff = n - num[len(num)-1]
+	sort.Sort(sort.Reverse(sort.IntSlice(num)))
+	fmt.Printf("%+v\n", num)
 	for i := range num {
-		if num[i] < n {
-			return i + 1
+		if num[i] <= n {
+			fmt.Printf("%d: %d -> %d\n", i, n, num[i])
+			return i + 1, diff
 		}
 	}
-	return len(num) + 1
+	// 最下位
+	return len(num) + 1, diff
 }
