e0af6a12ac26881d5291a5dfdef8562433897fcd
numbp <o9o73286699@gmail.com>
2020-01-20 18:37:06 +0900

    make struct into data

--- snippet/scan.go
+++ snippet/scan.go
@@ -8,12 +8,11 @@ import (
 func Scanf(n int) (count int, s string) {
 	str := make([]string, n)
 	for i := 0; i < n; i++ {
-		_, err := fmt.Scan(&s)
+		_, err := fmt.Scan(&str[i])
 		if err != nil {
 			continue
 		}
-		count += len([]rune(s))
-		str[i] = s
+		count += len([]rune(str[i]))
 	}
 
 	return count + n - 1, strings.Join(str, " ")
